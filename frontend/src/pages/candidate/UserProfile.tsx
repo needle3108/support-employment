2634f6fae19c5a94b3593a7f05ba6a90d8faf9ba
needle3108 <92887393+needle3108@users.noreply.github.com>
2024-11-20 15:46:01 +0100

    User profile page has been styled.

--- frontend/src/pages/candidate/UserProfile.tsx
+++ frontend/src/pages/candidate/UserProfile.tsx
@@ -2,45 +2,19 @@ import {Avatar, Box, Card, Typography} from "@mui/material";
 import UserNavbar from "../../components/UserNavbar";
 import React, {useEffect, useState} from "react";
 import {getAuthToken} from "../../services/BackendService";
-
-const cardStyle = {
-    position: 'absolute',
-    top: '12%',
-    left: '5%',
-    right: '52%',
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-}
+import {grey} from "@mui/material/colors";
+import { format } from 'date-fns'
 
 const avatarStyle = {
-    width: '230px',
-    height: '230px',
+    width: '200px',
+    height: '200px',
+    mt: '10px',
     margin: 'auto',
-    mt: '10px'
 }
 
 const typographyStyle = {
     mb: '5px'
 }
-
-const boxStyle = {
-    position: 'absolute',
-    left: '52%',
-    right: '5%',
-    top: '12%',
-    display: 'flex',
-    flexWrap: 'wrap'
-}
-
-const cardStyle2 = {
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-    width: '200px',
-    height: '280px',
-    mr: '13px',
-    mb: '13px',
-    textAlign: 'center',
-    flexShrink: '0',
-}
-
 
 export default function UserProfile() {
     const[firstName, setFirstName] = useState("");
@@ -98,33 +72,79 @@ export default function UserProfile() {
     }, [])
 
     return (
-        <Box>
+        <Box sx={{bgcolor: grey[200], height: '100%', width: '100%', left: 0, top: 0, overflow: 'auto', position: 'fixed'}}>
             <UserNavbar />
-            <Box>
-                <Card sx={cardStyle}>
-                    <Avatar src={"data:image/png;base64,"+file} sx={avatarStyle}></Avatar>
-                    <label>Imię: </label>
-                    <Typography variant="subtitle1" sx={typographyStyle}>{firstName}</Typography>
-                    <label>Nazwisko: </label>
-                    <Typography variant="subtitle1" sx={typographyStyle}>{lastName}</Typography>
-                    <label>Email: </label>
-                    <Typography variant="subtitle1" sx={typographyStyle}>{email}</Typography>
-                    <label>Numer telefonu: </label>
-                    <Typography variant="subtitle1" sx={typographyStyle}>{phoneNumber}</Typography>
-                    <label>Opis: </label>
-                    <Typography variant="subtitle1" sx={typographyStyle}>{description}</Typography>
-                    <label>Profesja: </label>
-                    <Typography variant="subtitle1" sx={typographyStyle}>{profession}</Typography>
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 4}}>
+                <Card sx={{
+                    position: 'relative',
+                    padding: 1,
+                    width: '750px',
+                    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                    overflow: 'hidden',
+                    '&:hover': { background: 'radial-gradient(circle, rgba(211,185,227,1) 0%, rgba(210,210,210,1) 100%, rgba(0,212,255,1) 100%)'},
+                }}>
+                    <Box sx={{display: 'flex'}}>
+                        <Box sx={{
+                            width: '40%',
+                            overflow: 'hidden',
+                            padding: 1,
+                            minWidth: '200px',
+                        }}>
+                            <Avatar src={"data:image/png;base64,"+file} sx={avatarStyle}></Avatar>
+                        </Box>
+                        <Box sx={{
+                            width: '60%',
+                            overflow: 'hidden',
+                            padding: 2,
+                            minWidth: '400px',
+                        }}>
+                            <Typography variant="h6" sx={typographyStyle}>Imię: {firstName}</Typography>
+                            <Typography variant="h6" sx={typographyStyle}>Nazwisko: {lastName}</Typography>
+                            <Typography variant="h6" sx={typographyStyle}>Email: {email}</Typography>
+                            <Typography variant="h6" sx={typographyStyle}>Numer kontaktowy: {phoneNumber}</Typography>
+                            <Typography variant="h6" sx={typographyStyle}>Profesja: {profession}</Typography>
+                        </Box>
+                    </Box>
+                    <Box sx={{margin: 1, mt: 2}}>
+                        <Typography variant="body1" sx={{wordWrap: 'normal'}}>{description}</Typography>
+                    </Box>
                 </Card>
             </Box>
-            <Box sx={boxStyle}>
+            <Box sx={{
+                mt: 8,
+            }}>
                 {
                     opinions.map(opinion => (
-                        <Card key={opinion["id"]} sx={cardStyle2}>
-                            <Avatar src={"data:image/png;base64,"+opinion["image"]}/>
-                            <Typography variant="h6" sx={typographyStyle}>{opinion["name"]} {opinion["lastName"]} | {opinion["companyName"]}</Typography>
-                            <Typography variant="subtitle2" sx={typographyStyle}>{opinion["dateTime"]}</Typography>
-                            <Typography variant="subtitle1" sx={typographyStyle}>{opinion["opinion"]}</Typography>
+                        <Card key={opinion["id"]} sx={{
+                            position: 'relative',
+                            ml: '10%',
+                            mr: '10%',
+                            mb: 2,
+                            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                            '&:hover': { background: 'radial-gradient(circle, rgba(211,185,227,1) 0%, rgba(210,210,210,1) 100%, rgba(0,212,255,1) 100%)'},
+                        }}>
+                            <Box sx={{display: 'flex'}}>
+                                <Avatar src={"data:image/png;base64,"+opinion["image"]} sx={{
+                                   margin: 1,
+                                    height: '35px',
+                                    width: '35px'
+                                }}/>
+                                <Typography variant="h6" sx={{
+                                    mt: 1.5
+                                }}>
+                                    {opinion["name"]} {opinion["lastName"]} | {opinion["companyName"]}
+                                </Typography>
+                            </Box>
+                            <Typography variant="body1" sx={{
+                                ml: 3,
+                                mt: 0.5,
+                                mb: 2
+                            }}>
+                                {opinion["opinion"]}
+                            </Typography>
+                            <Typography variant="caption" sx={{
+                                ml: 1,
+                            }}>{format(opinion["dateTime"],"d.MM.yyyy H:mm")}</Typography>
                         </Card>
                     ))
                 }
